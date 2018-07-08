c93ae7187b99c56088c8ddd689687814fb84c932
jbbn <joaobbn@gmail.com>
2018-07-08 02:28:22 -0300

    Keep going (languages: French)

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -29,4 +29,11 @@ func TestHello(t *testing.T) {
 	    assertCorrectMessage(t, got, want)
     })
 
+
+    t.Run("in French", func(t *testing.T) {
+	    got := Hello("Deroo", "French")
+	    want := "Bonjour, Deroo"
+	    assertCorrectMessage(t, got, want)
+    })
+
 }
